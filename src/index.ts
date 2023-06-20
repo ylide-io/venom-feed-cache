7e61985524fc219f14c11a27cc2768cdf9f91e75
Danila Simonov <ek@ek-mail.ru>
2023-06-20 13:46:48 +0200

    more good words

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,47 @@ const env = dotenv.config().parsed || {};
 
 import { startReader } from './reader';
 import { AppDataSource } from './database';
-import { startParser } from './parser';
+import { getNotGoodWords, isGoodPost, startParser } from './parser';
+import { DataSource, MoreThan } from 'typeorm';
+import { VenomFeedPostEntity } from './entities/VenomFeedPost.entity';
+import { asyncDelay } from '@ylide/sdk';
+
+async function doGoodPosts(db: DataSource) {
+	const rep = db.getRepository(VenomFeedPostEntity);
+	let lastTimestmap = 0;
+	while (true) {
+		const post = await rep.findOne({
+			where: {
+				banned: false,
+				isApproved: false,
+				isPredefined: false,
+				isAutobanned: false,
+				createTimestamp: MoreThan(lastTimestmap),
+			},
+			order: {
+				createTimestamp: 'ASC',
+			},
+		});
+		if (!post) {
+			return;
+		}
+		if (isGoodPost(post.contentText)) {
+			post.isApproved = true;
+			await rep.save(post);
+			console.log('Good post found: ', post.contentText);
+		} else {
+			lastTimestmap = post.createTimestamp;
+			console.log(
+				'Bad post: ' + post.contentText,
+				' bad words: ' +
+					getNotGoodWords(post.contentText)
+						.map(t => JSON.stringify(t))
+						.join(', '),
+			);
+			await asyncDelay(1000);
+		}
+	}
+}
 
 async function run() {
 	console.log('Start');
@@ -14,6 +54,7 @@ async function run() {
 		predefinedTexts: [],
 		bannedAddresses: [],
 	};
+	// await doGoodPosts(pool);
 	const updateCache = await startReader(sharedData, Number(env.PORT), pool);
 	await startParser(sharedData, updateCache, env.READ_FEED === 'true');
 }
